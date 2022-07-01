8d3c403b28b6831be115abcb72080c65e0401b36
shubhajitbasak <shubhajit.basak@yahoo.com>
2022-07-01 13:10:42 +0100

    commit@Jul22

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # prnet_new
-prnet re-implementation
+
+Code mostly copied and followed - 
+
+https://github.com/heathentw/prnet-tf2
+
+
+
+Download the BFM.mat and BFM_UV.mat and put in /utils/ folder -
+[BFM](https://drive.google.com/file/d/1eQzpKKJJnc2MSBo7X-OTgyxibinQnMjL/view?usp=sharing)
+
+Make sude to install mesh_core_cython in the local python environment 
+
+Results are not up to the mark, still work in progress!! :(
+
+Related Repo - 
+
+TF - 
+
+https://github.com/jnulzl/PRNet-Train
+
+
+torch - 
+
+https://github.com/reshow/PRNet-PyTorch
+
+https://github.com/tomguluson92/PRNet_PyTorch
+
+
+--------
+### Citation
+
+If you use this code, please consider citing:
+
+```
+@inProceedings{feng2018prn,
+  title     = {Joint 3D Face Reconstruction and Dense Alignment with Position Map Regression Network},
+  author    = {Yao Feng and Fan Wu and Xiaohu Shao and Yanfeng Wang and Xi Zhou},
+  booktitle = {ECCV},
+  year      = {2018}
+}
+```
---
+++ test.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+
+def test(self, error_func_list=None, is_visualize=False):
+    from demorender import demoAll
+    total_task = len(self.test_data)
+    print('total img:', total_task)
+
+    model = self.net.model
+    total_error_list = []
+    num_output = self.mode[3]
+    num_input = self.mode[4]
+    data_generator = DataGenerator(all_image_data=self.test_data, mode=self.mode[2], is_aug=False, is_pre_read=self.is_pre_read)
+
+    with torch.no_grad():
+        model.eval()
+        for i in range(len(self.test_data)):
+            data = data_generator.__getitem__(i)
+            x = data[0]
+            x = x.to(self.net.device).float()
+            y = [data[j] for j in range(1, 1 + num_input)]
+            for j in range(num_input):
+                y[j] = y[j].to(x.device).float()
+                y[j] = torch.unsqueeze(y[j], 0)
+            x = torch.unsqueeze(x, 0)
+            outputs = model(x, *y)
+
+            p = outputs[-1]
+            x = x.squeeze().cpu().numpy().transpose(1, 2, 0)
+            p = p.squeeze().cpu().numpy().transpose(1, 2, 0) * 280
+            b = sio.loadmat(self.test_data[i].bbox_info_path)
+            gt_y = y[0]
+            gt_y = gt_y.squeeze().cpu().numpy().transpose(1, 2, 0) * 280
+
+            temp_errors = []
+            for error_func_name in error_func_list:
+                error_func = getErrorFunction(error_func_name)
+                error = error_func(gt_y, p, b['Bbox'], b['Kpt'])
+                temp_errors.append(error)
+            total_error_list.append(temp_errors)
+            print(self.test_data[i].init_image_path, end='  ')
+            for er in temp_errors:
+                print('%.5f' % er, end=' ')
+            print('')
+            if is_visualize:
+
+                if temp_errors[0] > 0.00:
+                    tex = np.load(self.test_data[i].texture_path.replace('zeroz2', 'full')).astype(np.float32)
+                    init_image = np.load(self.test_data[i].cropped_image_path).astype(np.float32) / 255.0
+                    show([p, tex, init_image], mode='uvmap')
+                    init_image = np.load(self.test_data[i].cropped_image_path).astype(np.float32) / 255.0
+                    show([gt_y, tex, init_image], mode='uvmap')
+                    demobg = np.load(self.test_data[i].cropped_image_path).astype(np.float32)
+                    init_image = demobg / 255.0
+                    img1, img2 = demoAll(p, demobg, is_render=False)
+            mean_errors = np.mean(total_error_list, axis=0)
+            for er in mean_errors:
+                print('%.5f' % er, end=' ')
+            print('')
+        for i in range(len(error_func_list)):
+            print(error_func_list[i], mean_errors[i])
+
+        se_idx = np.argsort(np.sum(total_error_list, axis=-1))
+        se_data_list = np.array(self.test_data)[se_idx]
+        se_path_list = [a.cropped_image_path for a in se_data_list]
+        sep = '\n'
+        fout = open('errororder.txt', 'w', encoding='utf-8')
+        fout.write(sep.join(se_path_list))
+        fout.close()
